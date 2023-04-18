c9f8ddbe0ede53b1aa2d3a2db373830b9cd9f001
Kyng Cytro <johndibashi2001@gamil.com>
2023-04-18 04:32:22 +0100

    :tada: init

---
+++ README.md
@@ -0,0 +1,17 @@
+---
+title: Interest Protocol Discord Bot
+description: Swap, Lend, Trade and Farm on the Next Generation DeFi protocol ⚡|
+tags:
+  - discord.js
+  - typescript
+---
+
+## Interest Protocol
+
+Swap, Lend, Trade and Farm on the Next Generation DeFi protocol ⚡|
+
+## ✨ Features
+
+- Discord.js
+- TypeScript
+- Live-reload development
---
+++ src/index.ts
@@ -0,0 +1,79 @@
+import {
+  Client,
+  Events,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+  Partials,
+  REST,
+  Routes,
+  Collection,
+} from "discord.js";
+
+import type { SlashCommand } from "./types";
+import dotenv from "dotenv";
+dotenv.config();
+import testCommand from "./slashCommands/ping";
+import getStarted from "./slashCommands/getStarted";
+
+const token = process.env.DISCORD_TOKEN;
+const client_id = process.env.CLIENT_ID;
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
+  partials: [Partials.Channel],
+});
+
+client.once(Events.ClientReady, async (c) => {
+  console.log(`Logged in as ${c.user.tag}`);
+});
+
+const slashCommands = new Collection<string, SlashCommand>();
+
+slashCommands.set(testCommand.command.name, testCommand);
+slashCommands.set(getStarted.command.name, getStarted);
+
+const slashCommandsArr: SlashCommandBuilder[] = [
+  testCommand.command,
+  getStarted.command,
+];
+
+const rest = new REST({ version: "10" }).setToken(token);
+rest
+  .put(Routes.applicationCommands(client_id), {
+    body: slashCommandsArr.map((command) => command.toJSON()),
+  })
+  .then((data: any) => {
+    console.log(`🔥 Successfully loaded ${data.length} slash command(s)`);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  const command = slashCommands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
+client
+  .login(token)
+  .catch((error) => console.error("Discord.Client.Login.Error", error));
