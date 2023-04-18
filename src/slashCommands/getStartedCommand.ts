2006ecd5308ebc74892c0baa2e64f00e7e80c165
Kyng Cytro <johndibashi2001@gamil.com>
2023-04-18 05:16:54 +0100

    dep(prisma): added & setup prisma

--- src/slashCommands/getStartedCommand.ts
+++ src/slashCommands/getStartedCommand.ts
@@ -1,12 +1,17 @@
 import { SlashCommandBuilder } from "discord.js";
 import { SlashCommand } from "../types";
+import prisma from "../lib/prisma";
 
 const getStarted: SlashCommand = {
   command: new SlashCommandBuilder()
     .setName("get-started")
     .setDescription("Start earning from interest protocol"),
   execute: async (interaction) => {
-    interaction.reply({ content: "only you should see this", ephemeral: true });
+    const users = await prisma.user.findMany({});
+    interaction.reply({
+      content: `only you should see this ${users.map((user) => user.id)}`,
+      ephemeral: true,
+    });
   },
 };
 
