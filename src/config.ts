26c78ac64acb4c7b2438189373cb3436e2c04672
Kyng Cytro <johndibashi2001@gamil.com>
2023-04-19 08:54:40 +0100

    get-started command

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,15 @@
 import dotenv from "dotenv";
+import { z } from "zod";
+
 dotenv.config();
 
-export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+const enviromentVarsSchema = z.object({
+  DISCORD_TOKEN: z.string(),
+  CLIENT_ID: z.string(),
+  ZEALY_API_KEY: z.string(),
+  ZEALY_SUBDOMAIN: z.string(),
+});
+
+const enviromentVars = enviromentVarsSchema.parse(process.env);
+
+export default enviromentVars;
